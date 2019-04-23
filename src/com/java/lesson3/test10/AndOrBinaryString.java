4be191e377934ccfe2cca3c56b2d404e42c9f495
xiarui <28723656@qq.com>
2019-04-23 11:40:11 +0800

    add:firstCommit

---
+++ src/com/java/lesson3/test10/AndOrBinaryString.java
@@ -0,0 +1,25 @@
+package com.java.lesson3.test10;
+
+public class AndOrBinaryString {
+    public static void main(String[] args) {
+        Integer num1 = 0x23456;
+        Integer num2 = 0x34567;
+
+        System.out.println("num1:\t\t\t"+Integer.toBinaryString(num1));
+        System.out.println("num2:\t\t\t"+Integer.toBinaryString(num2));
+
+        // 全1为1，否则为0
+        Integer numAnd = num1&num2;
+        // 全0为0，否则为1
+        Integer numOr = num1|num2;
+        // 相同为0，不同为1
+        Integer numNotOr = num1^num2;
+        // 按位取反
+        Integer numNot = ~num1;
+
+        System.out.println("num1 & num2:\t"+Integer.toBinaryString(numAnd));
+        System.out.println("num1 | num2:\t"+Integer.toBinaryString(numOr));
+        System.out.println("num1 ^ num2:\t"+Integer.toBinaryString(numNotOr));
+        System.out.println("~num1:\t"+Integer.toBinaryString(numNot));
+    }
+}
---
+++ src/com/java/lesson3/test11/LeftLeft.java
@@ -0,0 +1,13 @@
+package com.java.lesson3.test11;
+
+public class LeftLeft {
+    public static void main(String[] args) {
+        Integer numberBefore = 20;
+        Integer numberAfter = numberBefore << 2;
+
+        System.out.println(numberBefore+"的二进制表示："+Integer.toBinaryString(numberBefore));
+        System.out.println("向左位移2位的二进制表示："+Integer.toBinaryString(numberAfter));
+
+    }
+}
+
